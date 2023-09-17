db45c9d98cf87167a7a0ae6befc353bbccf230bb
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-17 14:39:44 -0700

    Weekend and past date validation added to days

--- src/validators/days/validator.days.js
+++ src/validators/days/validator.days.js
@@ -7,6 +7,9 @@ const DaysValidations = () => {
         const { name, date } = data
         if (!name.match(CSVDOT_HYPHEN) || name.length === 0) return { error: 'Name is required' }
         if (!moment(date).isValid()) return { error: 'Invalid date chosen' }
+        if (moment(date).isBefore(moment(new Date()))) return { error: 'Cannot select a past dates' }
+        const selectedDay = moment(date).day()
+        if ((selectedDay === 6) || (selectedDay === 0)) return { error: 'Cannot select a weekend' }
         next()
     }
     return {
