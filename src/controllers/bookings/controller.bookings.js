eec3eef20230707c26ce7617895eb37507784cb7
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-24 19:59:30 -0700

    checking for crush source-15

--- src/controllers/bookings/controller.bookings.js
+++ src/controllers/bookings/controller.bookings.js
@@ -1,38 +1,38 @@
-import DatabaseConnection from "../../config/config.db.js"
-import { Regex } from "../../utils/static/index.js"
-import BookingsQuery from "../../queries/query.bookings.js"
-import BookingsValidations from "../../validators/bookings/validator.bookings.js"
-import RequestBodyChecker from "../../helpers/helper.request_checker.js"
-import RequestInformation from "../../helpers/helper.request_sender.js"
-import url from "url"
-import { ObjectId } from "bson"
+// import DatabaseConnection from "../../config/config.db.js"
+// import { Regex } from "../../utils/static/index.js"
+// import BookingsQuery from "../../queries/query.bookings.js"
+// import BookingsValidations from "../../validators/bookings/validator.bookings.js"
+// import RequestBodyChecker from "../../helpers/helper.request_checker.js"
+// import RequestInformation from "../../helpers/helper.request_sender.js"
+// import url from "url"
+// import { ObjectId } from "bson"
 
-export default function BookingControllers() {
-    const { pool } = DatabaseConnection()
-    const WSWW = 'Whoops! Something went wrong'
-    const regex = Regex
-    const bookingQueries = BookingsQuery()
-    const validations = BookingsValidations()
-    const { isTrueBodyStructure } = RequestBodyChecker()
+// export default function BookingControllers() {
+//     const { pool } = DatabaseConnection()
+//     const WSWW = 'Whoops! Something went wrong'
+//     const regex = Regex
+//     const bookingQueries = BookingsQuery()
+//     const validations = BookingsValidations()
+//     const { isTrueBodyStructure } = RequestBodyChecker()
 
 
-    const getBookingRequirements = async (req, res) => {
+//     const getBookingRequirements = async (req, res) => {
 
-    }
+//     }
 
-    const getBookingSlots = (req, res) => {
+//     const getBookingSlots = (req, res) => {
 
-    }
+//     }
 
-    const EquipmentBooking = async (res, userId, equipment_id, date, need_assist, slots) => {
+//     const EquipmentBooking = async (res, userId, equipment_id, date, need_assist, slots) => {
 
-    }
+//     }
 
-    const bookEquipment = (req, res) => {
+//     const bookEquipment = (req, res) => {
 
-    }
+//     }
 
-    return {
-        getBookingRequirements, getBookingSlots, bookEquipment
-    }
-}
+//     return {
+//         getBookingRequirements, getBookingSlots, bookEquipment
+//     }
+// }
--- src/routes/router.bookings.js
+++ src/routes/router.bookings.js
@@ -1,13 +1,15 @@
 import express from 'express'
-import UserMiddleware from '../middlewares/middleware.user.js'
-import BookingControllers from '../controllers/bookings/controller.bookings.js'
+// import UserMiddleware from '../middlewares/middleware.user.js'
+// import BookingControllers from '../controllers/bookings/controller.bookings.js'
 
 const bookingRouter = express.Router()
 
-const bookingsControllers = BookingControllers()
-
-bookingRouter.get('/requirements', UserMiddleware, bookingsControllers.getBookingRequirements)
-bookingRouter.get('/slots', UserMiddleware, bookingsControllers.getBookingSlots)
-bookingRouter.post('/', UserMiddleware, bookingsControllers.bookEquipment)
+// const bookingsControllers = BookingControllers()
+bookingRouter.get('/', (req, res) => {
+    return res.status(200).json('aegafa')
+})
+// bookingRouter.get('/requirements', UserMiddleware, bookingsControllers.getBookingRequirements)
+// bookingRouter.get('/slots', UserMiddleware, bookingsControllers.getBookingSlots)
+// bookingRouter.post('/', UserMiddleware, bookingsControllers.bookEquipment)
 
 export default bookingRouter
