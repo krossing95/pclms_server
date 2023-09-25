facd9fd96fcf435e94584fb69ad05e39457aa6aa
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-24 18:48:48 -0700

    checking for crush source-4

---
+++ src/controllers/bookings/controller.bookings.js
@@ -0,0 +1,17 @@
+export default function BookingControllers() {
+    const getRequirements = async (req, res) => {
+
+    }
+
+    const getSlots = async (req, res) => {
+
+    }
+
+    const bookEquipment = async (req, res) => {
+
+    }
+
+    return {
+        getRequirements, getSlots, bookEquipment
+    }
+}
--- src/routes/router.bookings.js
+++ src/routes/router.bookings.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import UserMiddleware from '../middlewares/middleware.user.js'
+import BookingControllers from '../controllers/bookings/controller.bookings.js'
 
 const bookingRouter = express.Router()
 
-bookingRouter.get('/', (req, res) => {
-    console.log(true);
-})
+const bookingsControllers = BookingControllers()
+
+bookingRouter.get('/requirements', UserMiddleware, bookingsControllers.getRequirements)
+bookingRouter.get('/slots', UserMiddleware, bookingsControllers.getSlots)
+bookingRouter.post('/', UserMiddleware, bookingsControllers.bookEquipment)
 
 export default bookingRouter
