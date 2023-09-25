ce5f65762d2c2f9db563516cabe3af5ae320b404
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-24 21:21:34 -0700

    restarting equipment booking--2

--- src/controllers/equipment_booking/controller.equipment_booking.js
+++ src/controllers/equipment_booking/controller.equipment_booking.js
@@ -1,6 +1,44 @@
+import DatabaseConnection from "../../config/config.db.js"
+import url from "url"
+import { Regex } from "../../utils/static/index.js"
+import RequestBodyChecker from "../../helpers/helper.request_checker"
+import BookingsQuery from "../../queries/query.bookings.js"
+import BookingsValidations from "../../validators/bookings/validator.bookings.js"
+import RequestInformation from "../../helpers/helper.request_sender.js"
+
 const EquipmentBookingControllers = () => {
+    const { pool } = DatabaseConnection()
+    const WSWW = 'Whoops! Something went wrong'
+    const regex = Regex
+    const bookingQueries = BookingsQuery()
+    const validations = BookingsValidations()
+    const { isTrueBodyStructure } = RequestBodyChecker()
+
+
     const getBookingRequirements = async (req, res) => {
-        return res.json({ ...req.body })
+        const params = new URLSearchParams(url.parse(req.url, true).query)
+        if (!params.get('equipment_id')) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
+        const equipment_id = params.get('equipment_id')
+        if (!equipment_id.match(regex.MONGOOBJECT)) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
+        try {
+            const blockedDays = (await pool.query(bookingQueries.GETBLOCKEDDAYS)).rows
+            let bookedDays = (await pool.query(bookingQueries.GETBOOKEDDAYS, [equipment_id, 3])).rows
+            if (bookedDays.length === 0) return res.status(200).json({
+                message: '', code: '200', data: {
+                    unavailable_days: [...blockedDays]
+                }
+            })
+            bookedDays = bookedDays.filter(day => day.slots.length < 18).map(day => {
+                return { ...day, name: 'Booked' }
+            })
+            return res.status(200).json({
+                message: '', code: '200', data: {
+                    unavailable_days: [...bookedDays, ...blockedDays]
+                }
+            })
+        } catch (error) {
+            return res.status(500).json({ message: WSWW, code: '500', data: {} })
+        }
     }
 
 
