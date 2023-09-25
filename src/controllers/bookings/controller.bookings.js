6a0c5b56b79410ab1eb16f4b5be2d98bac7cd4a2
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-24 19:02:30 -0700

    checking for crush source-7

--- src/controllers/bookings/controller.bookings.js
+++ src/controllers/bookings/controller.bookings.js
@@ -16,24 +16,24 @@ export default function BookingControllers() {
 
 
     const getRequirements = async (req, res) => {
-        const params = new URLSearchParams(url.parse(req.url, true).query)
-        if (!params.get('equipment_id')) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
-        const equipment_id = params.get('equipment_id')
-        if (!equipment_id.match(regex.MONGOOBJECT)) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
-        try {
-            const blockedDays = (await pool.query(bookingQueries.GETBLOCKEDDAYS)).rows
-            let bookedDays = (await pool.query(bookingQueries.GETBOOKEDDAYS, [equipment_id, 3])).rows
-            bookedDays = bookedDays.length > 0 ? bookedDays.filter(day => day.slots.length < 18).map(day => {
-                return { ...day, name: 'Booked' }
-            }) : []
-            return res.status(200).json({
-                message: '', code: '200', data: {
-                    unavailable_days: [...bookedDays, ...blockedDays]
-                }
-            })
-        } catch (error) {
-            return res.status(500).json({ message: WSWW, code: '500', data: {} })
-        }
+        // const params = new URLSearchParams(url.parse(req.url, true).query)
+        // if (!params.get('equipment_id')) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
+        // const equipment_id = params.get('equipment_id')
+        // if (!equipment_id.match(regex.MONGOOBJECT)) return res.status(412).json({ message: 'Bad request', code: '412', data: {} })
+        // try {
+        //     const blockedDays = (await pool.query(bookingQueries.GETBLOCKEDDAYS)).rows
+        //     let bookedDays = (await pool.query(bookingQueries.GETBOOKEDDAYS, [equipment_id, 3])).rows
+        //     bookedDays = bookedDays.length > 0 ? bookedDays.filter(day => day.slots.length < 18).map(day => {
+        //         return { ...day, name: 'Booked' }
+        //     }) : []
+        //     return res.status(200).json({
+        //         message: '', code: '200', data: {
+        //             unavailable_days: [...bookedDays, ...blockedDays]
+        //         }
+        //     })
+        // } catch (error) {
+        //     return res.status(500).json({ message: WSWW, code: '500', data: {} })
+        // }
     }
 
     const getSlots = (req, res) => {
