20fd7a4e46a2c8c0ecc20ee965d1ae8e8f362ca8
Krossing95 <kalenuxoesekwasi@gmail.com>
2023-09-24 18:32:13 -0700

    checking for crush source

--- src/routes/router.bookings.js
+++ src/routes/router.bookings.js
@@ -4,7 +4,7 @@ import UserMiddleware from '../middlewares/middleware.user.js'
 
 const bookingRouter = express.Router()
 
-const bookingsControllers = BookingControllers()
+// const bookingsControllers = BookingControllers()
 
 // bookingRouter.get('/requirements', UserMiddleware, bookingsControllers.getRequirements)
 // bookingRouter.get('/slots', UserMiddleware, bookingsControllers.getSlots)
